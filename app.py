a6aef5b892a000301dd688e64744681b32f31000
ACitronella <phuriwat.13042@gmail.com>
2023-04-21 23:33:11 +0700

    add preprocessing to resize image to 600, 600/img.width*img.height

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from io import BytesIO
 from PIL import Image
 import numpy as np
 from people import PeopleCollection
-from util import locate_faces_in_image
+from util import locate_faces_in_image, preprocessing_image_pil
 from typing import Union, Optional
 people = PeopleCollection(ids=[], imgs=[])
 app = FastAPI()
@@ -11,27 +11,29 @@ app = FastAPI()
 
 @app.post("/face_registeration")
 async def face_registeration(id:str = Form(...), file: UploadFile = File(...)) -> dict[str, Union[bool, Optional[str], tuple[tuple[int, int], tuple[int, int]]]]:
-    contents = await file.read() # <-- Important!
-    img = Image.open(BytesIO(contents))
+    contents = await file.read()
+    img = Image.open(BytesIO(contents)).convert("RGB")
+    img = preprocessing_image_pil(img)
     img = np.array(img)
     img_h, img_w, _ = img.shape
+    face_locations = locate_faces_in_image(img)
+    if len(face_locations) == 0:
+        return {"success": False, "id": None, "loc": None, "message": "face not found in the image"} 
+    y0, x0, y1, x1 = face_locations[0]
+    x_min, x_max = (x0, x1) if x0 < x1 else (x1, x0)
+    y_min, y_max = (y0, y1) if y0 < y1 else (y1, y0)
+    y_min, y_max, x_min, x_max = max(y_min-100, 0), min(y_max+100, img_h), max(x_min-100, 0), min(x_max+100, img_w)
     try:
-        face_locations = locate_faces_in_image(img)
-        y0, x0, y1, x1 = face_locations[0] # index error
-        x_min, x_max = (x0, x1) if x0 < x1 else (x1, x0)
-        y_min, y_max = (y0, y1) if y0 < y1 else (y1, y0)
-        y_min, y_max, x_min, x_max = max(y_min-100, 0), min(y_max+100, img_h), max(x_min-100, 0), min(x_max+100, img_w)
         people.add_new_faces([id], [img[y_min:y_max, x_min:x_max]]) # assertion Error
     except AssertionError as e:
-        return {"success": False, "id": None, "loc": None, "message": str(e)} 
-    except IndexError as e:
         return {"success": False, "id": None, "loc": None, "message": str(e)} 
     return {"success": True, "id": id, "loc": ((x_min, y_min), (x_max, y_max))} 
 
 @app.post("/face_recognition")
 async def face_recognition(file: UploadFile = File(...)) -> dict[str, Union[bool, str, list[dict[str, Union[str, tuple[tuple[int, int], tuple[int, int]]]]]]]:
     contents = await file.read() # <-- Important!
-    img = Image.open(BytesIO(contents))
+    img = Image.open(BytesIO(contents)).convert("RGB")
+    img = preprocessing_image_pil(img)
     img = np.array(img)
     img_h, img_w, _ = img.shape
     face_locations = locate_faces_in_image(img)
@@ -47,6 +49,7 @@ async def face_recognition(file: UploadFile = File(...)) -> dict[str, Union[bool
     except AssertionError as e:
         return {"success": False, "found_face": [], "message": "face reconition failed " + str(e)} 
     return {"success": True, "found_faces": face_location_with_names, "message": "hopefully face was found here if it exists"}
+    
 @app.get("/")
 def hello_world():
     return "hello world"
--- util.py
+++ util.py
@@ -1,12 +1,17 @@
 import numpy as np
+import numpy.typing as npt
 from pathlib import Path
 from PIL import Image
 from typing import Union
 import face_recognition
 
-def image_reader(img_path: Union[str, Path]) -> np.ndarray:
+def image_reader(img_path: Union[str, Path]) -> npt.NDArray:
     return np.array(Image.open(img_path))
 
 def locate_faces_in_image(img: np.ndarray) -> list[tuple[int, int, int, int]]:
     return face_recognition.face_locations(img)
-    
+
+def preprocessing_image_pil(img: Image.Image) -> Image.Image: # resize to 500, 500/width * height to speed up the later processes
+    scale_ratio = 500/img.width
+    img = img.resize((500, int(scale_ratio * img.height)), Image.Resampling.LANCZOS)
+    return img
